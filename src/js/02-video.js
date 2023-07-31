83acd603d4ac849f198ef3f12810c76c489ae886
MykhailoYavorskyi <my70035@gmail.com>
2023-07-31 11:40:35 +0200

    02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef, {
+  fullscreen: true,
+  quality: '1080p',
+  loop: true,
+});
+
+const onPlay = function (data) {
+  console.log(data);
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
